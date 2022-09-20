d1a585b2338b0e1c4fdfb5888d7534189d337f53
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2022-09-20 09:22:30 +0300

    init commit

---
+++ NetologyHW_MapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetologyHW_MapKit
+//
+//  Created by Dima Gorbachev on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
