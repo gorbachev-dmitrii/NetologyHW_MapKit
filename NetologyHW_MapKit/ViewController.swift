51f6d35eb9cb15bf213d3337423ccab0542804d8
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2022-09-21 23:37:14 +0300

    created simple map

--- NetologyHW_MapKit/ViewController.swift
+++ NetologyHW_MapKit/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(mapView)
+        setupConstraints()
     }
-
-
+    
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+    }
 }
 
