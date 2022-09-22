b0d12487442452fc4b749428904a7e1308504f21
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2022-09-23 00:13:58 +0300

    implemented user location

--- NetologyHW_MapKit/ViewController.swift
+++ NetologyHW_MapKit/ViewController.swift
@@ -13,15 +13,23 @@ class ViewController: UIViewController {
     let mapView: MKMapView = {
         let map = MKMapView()
         map.translatesAutoresizingMaskIntoConstraints = false
+        map.showsScale = true
         return map
+    }()
+    
+    lazy var locationManager: CLLocationManager = {
+        let location = CLLocationManager()
+        location.delegate = self
+        location.desiredAccuracy = kCLLocationAccuracyBest
+        return location
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(mapView)
         setupConstraints()
+        checkLocationAuthStatus()
     }
-    
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
@@ -30,6 +38,34 @@ class ViewController: UIViewController {
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             mapView.topAnchor.constraint(equalTo: view.topAnchor)
         ])
+    }
+    
+    private func checkLocationAuthStatus() {
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            mapView.showsUserLocation = true
+        case .notDetermined:
+            locationManager.requestAlwaysAuthorization()
+        case .denied, .restricted:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    internal func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedAlways  {
+            manager.startUpdatingLocation()
+        }
+    }
+    
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        mapView.setCenter(locations.first!.coordinate, animated: true)
     }
 }
 
